7e5fba8f0f399f4c5da710d68539a8f762d31902
johnson1994 <starjohnson@sina.com>
2016-08-10 18:46:00 +0800

    re 8-10 fixed auto-reply bug.

--- api/src/main/java/cn/springmvc/service/impl/RefreshServiceImpl.java
+++ api/src/main/java/cn/springmvc/service/impl/RefreshServiceImpl.java
@@ -14,38 +14,28 @@ public class RefreshServiceImpl implements RefreshService {
 	Logger logger = Logger.getLogger(RefreshServiceImpl.class);
 
 	public void cacheRefresh() {
-/*		
-		// System.out.println("test OK");
-
-		String key = "access_token";
-
-		// String url =
-		// "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="
-		// + Consts.APP_ID
-		// + "&secret=" + Consts.APP_SERCRET;
-		String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="
-				+ Consts.getBASIC_DATA().getAppId() + "&secret=" + Consts.getBASIC_DATA().getAppSecret();
-
-		MemcacheUtil memcache = null;
-		String response = null;
-		String token = null;
-		try {
-			memcache = MemcacheUtil.getInstance();
-			response = RequestUtil.doGet(url);
-			token = ((Map<String, String>) JSON.parse(response)).get("access_token");
-			memcache.setDat(key, token);
-			logger.error("insert memcache success <<< " + token);
-		} catch (Exception e) {
-			e.printStackTrace();
-			logger.error("insert memcache error >>> " + e.getStackTrace());
-		} finally {
-			try {
-				memcache.destory();
-			} catch (IOException e) {
-				e.printStackTrace();
-				logger.error("memcache close error >>> " + e.getStackTrace());
-			}
-		}
-*/
+		/*
+		 * // System.out.println("test OK");
+		 * 
+		 * String key = "access_token";
+		 * 
+		 * // String url = //
+		 * "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="
+		 * // + Consts.APP_ID // + "&secret=" + Consts.APP_SERCRET; String url =
+		 * "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="
+		 * + Consts.getBASIC_DATA().getAppId() + "&secret=" +
+		 * Consts.getBASIC_DATA().getAppSecret();
+		 * 
+		 * MemcacheUtil memcache = null; String response = null; String token =
+		 * null; try { memcache = MemcacheUtil.getInstance(); response =
+		 * RequestUtil.doGet(url); token = ((Map<String, String>)
+		 * JSON.parse(response)).get("access_token"); memcache.setDat(key,
+		 * token); logger.error("insert memcache success <<< " + token); } catch
+		 * (Exception e) { e.printStackTrace();
+		 * logger.error("insert memcache error >>> " + e.getStackTrace()); }
+		 * finally { try { memcache.destory(); } catch (IOException e) {
+		 * e.printStackTrace(); logger.error("memcache close error >>> " +
+		 * e.getStackTrace()); } }
+		 */
 	}
 }
