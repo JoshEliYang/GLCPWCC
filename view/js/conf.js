8bfbdbdc12dfacd9f1aabc6aa41fc48cd20b928d
johnson1994 <starjohnson@sina.com>
2016-11-08 10:23:41 +0800

    re 11-8 Fixed conflict.

--- view/js/conf.js
+++ view/js/conf.js
@@ -1,13 +1,3 @@
-
-var basicUrl = "http://120.26.54.131:8080/GLCPWCC/basic/inusing";
-var basicUrlAll = "http://120.26.54.131:8080/GLCPWCC/basic/all";
-var basicSetUsingUrl = "http://120.26.54.131:8080/GLCPWCC/basic/setUsing";
-var basicSetDefaultUrl = "http://120.26.54.131:8080/GLCPWCC/basic/setDefault";
-var basicInsertUrl = "http://120.26.54.131:8080/GLCPWCC/basic/insert";
-var basicEditUrl = "http://120.26.54.131:8080/GLCPWCC/basic/edit";
-var basicDeleteUrl = "http://120.26.54.131:8080/GLCPWCC/basic/delete/";
-var selectTokenServerUrl = "http://120.26.54.131:8080/GLCPWCC/basic/setTokenServer";
-
 //////////////////////////////// http //////////////////////////////////////
 var loginUrl = "http://120.26.54.131:8080/GLCPWCC/login/login";
 
@@ -20,44 +10,6 @@ var basicEditUrl = "http://120.26.54.131:8080/GLCPWCC/basic/edit";
 var basicDeleteUrl = "http://120.26.54.131:8080/GLCPWCC/basic/delete/";
 var selectTokenServerUrl = "http://120.26.54.131:8080/GLCPWCC/basic/setTokenServer";
 
-var userInfoUrl = "http://120.26.54.131:8080/GLCPWCC/admin/info";
-var userUrlAll = "http://120.26.54.131:8080/GLCPWCC/admin/all";
-var userLevelsUrlAll = "http://120.26.54.131:8080/GLCPWCC/admin/allLevels";
-var userInsertUrl = "http://120.26.54.131:8080/GLCPWCC/admin/insert";
-var userEditUrl = "http://120.26.54.131:8080/GLCPWCC/admin/edit";
-var userDeleteUrl = "http://120.26.54.131:8080/GLCPWCC/admin/delete/";
-var resetPasswdUrl = "http://120.26.54.131:8080/GLCPWCC/admin/resetPasswd";
-var levelRightUrl = "http://120.26.54.131:8080/GLCPWCC/admin/levelRight";
-var addAdminLevelUrl = "http://120.26.54.131:8080/GLCPWCC/admin/addLevel";
-var adminLevelEditUrl = "http://120.26.54.131:8080/GLCPWCC/admin/userLevel";
-
-var buttonGroupUrl = "http://120.26.54.131:8080/GLCPWCC/button/group";
-var buttonsUrl = "http://120.26.54.131:8080/GLCPWCC/button/button/";
-
-var msgTypeUrl = "http://120.26.54.131:8080/GLCPWCC/msgtype/all";
-var keyWordsUrl = "http://120.26.54.131:8080/GLCPWCC/keywords/all";
-var keyWordsInsertUrl = "http://120.26.54.131:8080/GLCPWCC/keywords/insert";
-var keyWordsSetInUsing = "http://120.26.54.131:8080/GLCPWCC/keywords/setInUsing";
-var keyWordsEdit = "http://120.26.54.131:8080/GLCPWCC/keywords/edit";
-var deleteKeyWordsUrl = "http://120.26.54.131:8080/GLCPWCC/keywords/";
-var subscribeReplyUrl = "http://120.26.54.131:8080/GLCPWCC/keywords/subscribe";
-var insertSubscribeUrl = "http://120.26.54.131:8080/GLCPWCC/keywords/subscribe";
-var setSubscribeInUsingUrl = "http://120.26.54.131:8080/GLCPWCC/keywords/subscribe/setInUsing";
-
-var tagGetUrl = "http://120.26.54.131:8080/GLCPWCC/tag/get";
-var tagGetUserUrl = "http://120.26.54.131:8080/GLCPWCC/tag/getUser";
-var tagCreateUrl = "http://120.26.54.131:8080/GLCPWCC/tag/create";
-var tagDeleteUrl = "http://120.26.54.131:8080/GLCPWCC/tag/delete";
-var tagUpdateUrl = "http://120.26.54.131:8080/GLCPWCC/tag/update";
-var tagCreateWithQr = "http://120.26.54.131:8080/GLCPWCC/tag/tagAndQrcode";
-
-var qrcodeGetUrl = "http://120.26.54.131:8080/GLCPWCC/Qrcode/select";
-var qrcodeCreateUrl = "http://120.26.54.131:8080/GLCPWCC/Qrcode/create"
-var getResourceListUrl = "http://120.26.54.131:8080/GLCPWCC/resource/";
-var getImageUrl = "http://120.26.54.131:8080/GLCPWCC/test/wechatimage";
-
-var MessageExcelUrl = "http://120.26.54.131:8080/GLCPWCC/message/upload";
-var TemplateMessageButtonsUrl = "http://120.26.54.131:8080/GLCPWCC/message/templates";
 var userInfoUrl = "http://120.26.54.131:8080/GLCPWCC/admin/info";
 var userUrlAll = "http://120.26.54.131:8080/GLCPWCC/admin/all";
 var userLevelsUrlAll = "http://120.26.54.131:8080/GLCPWCC/admin/allLevels";
@@ -115,7 +67,6 @@ var getDebug = "http://120.26.54.131:8080/GLCPWCC/log/DebugMode";
 var MessageExcelUrl = "http://120.26.54.131:8080/GLCPWCC/message/upload/SendTemplateMessage";
 var TemplateMessageButtonsUrl = "http://120.26.54.131:8080/GLCPWCC/message/templates";
 var TicketExpiredUrl = "http://120.26.54.131:8080/GLCPWCC/message/upload/TicketExpiredMessage";
->>>>>>> 819e3c1ad71b09b57625c780ea4e2aadc82e49ea
 
 var socketUrl = "ws://120.26.54.131:8080/GLCPWCC/progress";
 
