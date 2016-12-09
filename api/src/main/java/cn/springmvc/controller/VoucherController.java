7ee0c5b36906736166b774898545597d762f8dca
summ <summwu@foxmail.com>
2016-12-09 09:17:06 +0800

    re: 12-9 add func vouche select API.

--- api/src/main/java/cn/springmvc/controller/VoucherController.java
+++ api/src/main/java/cn/springmvc/controller/VoucherController.java
@@ -5,11 +5,10 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,35 +16,32 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.springmvc.utils.HttpUtils;
 
 import cn.springmvc.model.BasicModel;
-import cn.springmvc.model.UserParamModel;
-import cn.springmvc.model.VoucherModel;
-import cn.springmvc.service.VoucherBuildingService;
+import cn.springmvc.model.QrcodeModel;
+import cn.springmvc.service.VoucherService;
 
 @Scope("prototype")
 @Controller
-@RequestMapping("/voucherBinding")
+@RequestMapping("/voucher")
+
 public class VoucherController {
-
 	@Autowired
-	public VoucherBuildingService voucherBuildingService;
-	Logger logger = Logger.getLogger(VoucherController.class);
-
+	VoucherService voucherSevice;
+	
+	/**
+	 * 剩余优惠券查询
+	 * @author summ
+	 */
 	@ResponseBody
-	@RequestMapping(value = "/user", method = RequestMethod.POST)
-	public Map<String, Object> create(@RequestBody VoucherModel vmodel, HttpServletRequest request) {
-		BasicModel model = (BasicModel) request.getAttribute("BasicModel");
-		// Map<String, String> result = null;
-
-		List<UserParamModel> result;
-
+	@RequestMapping(method = RequestMethod.GET)
+	public Map<String, Object> getAll() {
 		try {
-			result = voucherBuildingService.getUser(vmodel, model);
-			logger.error("tags--" + result);
-			return HttpUtils.generateResponse("0", "success", result);
+			return HttpUtils.generateResponse("0", "get vouche success", voucherSevice.getVouvher());
 		} catch (Exception e) {
-			e.printStackTrace();
-			logger.error("error");
-			return HttpUtils.generateResponse("1", "failed", null);
+			// TODO: handle exception
+			return HttpUtils.generateResponse("1", "get vouvher failed", "0");
 		}
+		
 	}
+
+
 }
---
+++ api/src/main/java/cn/springmvc/daoLvdi/VoucherDao.java
@@ -0,0 +1,10 @@
+package cn.springmvc.daoLvdi;
+
+import java.util.List;
+
+import cn.springmvc.model.BasicModel;
+import cn.springmvc.model.VoucheModel;
+
+public interface VoucherDao {
+	public List<VoucheModel> getVoucher();
+}
